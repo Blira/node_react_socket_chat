a8d99dc8f834407f61fa7f483bc03eb94c918895
Bruno Lira <brunoelira@gmail.com>
2021-04-21 21:13:41 -0300

    style: fix identation

--- client/src/components/MessageBox/style.ts
+++ client/src/components/MessageBox/style.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
     
 
     strong {
